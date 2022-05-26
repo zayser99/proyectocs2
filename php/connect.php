2deb1047286b32ad7dcfe5e097f7df8be6b13b11
ZAYSER99 <zayser99@hotmail.com>
2022-05-26 00:15:19 -0500

    se creo cla carpeta php

---
+++ php/connect.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+<?php
+	$mysqli= new mysqli("localhost","root","","energias_limpias");
+	if(mysqli_connect_errno()){
+		echo "no se pudo conectar";
+	}
+?>
+<body>
+</body>
+</html>
