5f396f93cf51ea66de1114ee61b80a93ae3a127e
ZAYSER99 <zayser99@hotmail.com>
2022-05-26 00:40:52 -0500

    plantilla para las listas

--- php/connect.php
+++ php/connect.php
@@ -5,7 +5,7 @@
 <title>Documento sin título</title>
 </head>
 <?php
-	$mysqli= new mysqli("localhost","root","","energias_limpias");
+	$mysqli= new mysqli("localhost","root","","pruebadbcs2");
 	if(mysqli_connect_errno()){
 		echo "no se pudo conectar";
 	}
--- php/row_profesores.php
+++ php/row_profesores.php
@@ -2,13 +2,14 @@
 <html lang="en">
 
 <head>
-    <title>row de actas</title>
+    <title>row de profesores</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS v5.0.2 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/main.css" >
 
 </head>
 
