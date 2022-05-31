7a07c637d4f1cccc9fd57d3e4de537bf5585ad54
ramon90 <ramon_ramirez90@hotmail.com>
2022-05-30 20:33:56 -0500

    imprimir

--- php/row_actas.php
+++ php/row_actas.php
@@ -17,7 +17,7 @@
     <div class="d-grid gap-2 col-6 mx-auto">
         <button class="btn btn-success" type="button" onclick="window.location.href='CRUD/actas/acta_agregar.php'">Nueva Acta</button>
     </div>
-
+    <button class="btn btn-success" type="button" onclick="window.location.href='../Index.php'">Regresar</button>
     <!-- consultamos los datos para meterlos en un objeto -->
     <?php
     $conexion = include_once "connect.php";
@@ -59,7 +59,15 @@
                             </td>
                             <td> <span class="badge rounded-pill bg-success"> <?php echo $acta["acta_fecha"] ?></span></td>
                             <td>
-                                <button type="button" class="btn btn-primary" onclick="window.location.href='../InicioAgregar.php? id=<?php echo $acta['acta_id'];?>'">
+                                <button type="button" class="btn btn-outline-warning" onclick="window.location.href='CRUD/actas/imprimir.php? id=<?php echo $acta['acta_id']; ?>'">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
+                                        <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
+                                        <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+                                    </svg>
+                                    Imprimir
+                                </button>
+
+                                <button type="button" class="btn btn-primary" onclick="window.location.href='../InicioAgregar.php? id=<?php echo $acta['acta_id']; ?>'">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
@@ -67,7 +75,7 @@
                                     Editar
                                 </button>
 
-                                <button type="button" class="btn btn-outline-danger" onclick="window.location.href='CRUD/actas/acta_eliminar.php? id=<?php echo $acta['acta_id'];?>'">
+                                <button type="button" class="btn btn-outline-danger" onclick="window.location.href='CRUD/actas/acta_eliminar.php? id=<?php echo $acta['acta_id']; ?>'">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
                                     </svg>
